370ef8c5234a9e135dc5289a37f31553ae2a686e
Kacper Drabicki <kdrabicki23@gmail.com>
2026-09-21 18:24:46 +0200

    replace get_batch with pytorch dataloader

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
+import time
 import json
 import torch
 import torch.nn.functional as F
 from datetime import datetime
 from pathlib import Path
 from dataclasses import asdict
+from torch.utils.data import Dataset, DataLoader
 from tokenizer import CharacterTokenizer
 from model import MiniGPT, GPTConfig
 
@@ -22,9 +24,10 @@ seed = 42
 dataset: str = 'input.txt'
 train_fraction = 0.9
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-batch_size: int = 64
+batch_size: int = 256
 learning_rate: float = 3e-4
-n_iters: int = 10000
+n_epochs: int = 3
+# n_iters: int = 10000 # works with get_batch() that was replaced with DataLoader; use n_epochs
 #------------------------------------------------------
 torch.manual_seed(seed)
 #------------------------------------------------------
@@ -55,12 +58,11 @@ with config_path.open('w') as f:
         'train_fraction': train_fraction,
         'batch_size': batch_size,
         'learning_rate': learning_rate,
-        'n_iters': n_iters,
+        'n_epochs': n_epochs,
         'model_config': asdict(config),
         'tokenizer_chars': tokenizer.chars}, f
         )
 #------------------------------------------------------
-
 def get_batch(split):
     data = train_data if split == 'train' else val_data
     idx = torch.randint(len(data) - config.block_size, (batch_size,))
@@ -68,6 +70,27 @@ def get_batch(split):
     y = torch.stack([data[i+1: i+config.block_size+1] for i in idx])
     x, y = x.to(device), y.to(device)
     return x, y
+#------------------------------------------------------
+class TextDataset(Dataset):
+    def __init__(self, data, block_size):
+        self.data = data
+        self.block_size = block_size
+
+    def __len__(self):
+        return len(self.data) - self.block_size
+
+    def __getitem__(self, idx):
+        x = self.data[idx:idx + self.block_size]
+        y = self.data[idx + 1:idx + self.block_size + 1]
+
+        return x, y
+
+train_dataset = TextDataset(train_data, config.block_size)
+val_dataset = TextDataset(val_data, config.block_size)
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+#------------------------------------------------------
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
@@ -76,42 +99,65 @@ def compute_loss(logits, targets):
     B, T, V = logits.shape # batch_size, sequence_length, vocab_size
     return F.cross_entropy(logits.view(B*T, V), targets.view(B*T))
 
-def log_metrics(step: int, train_loss: float, val_loss: float) -> None:
+def log_metrics(epoch: int, train_loss: float, val_loss: float) -> None:
     with metrics_path.open('a') as f:
         f.write(
             json.dumps({
-            'step': step + 1,
+            'epoch': epoch + 1,
             'train_loss': train_loss,
             'val_loss': val_loss
             }) + "\n")
 #------------------------------------------------------
-for step in range(n_iters):
+# train_iter = iter(train_loader)
+
+for epoch in range(n_epochs):
+
+    start = time.perf_counter()
 
     model.train()
 
-    x, y = get_batch('train')
-    logits = model(x)
-    loss = compute_loss(logits, y)
+    train_loss = 0.0
+    n_batches = 0
+    # x, y = get_batch('train')
+    # x, y = next(train_iter)
+    for x, y in train_loader:
+        x, y = x.to(device), y.to(device)
 
-    optimizer.zero_grad()
-    loss.backward()    
-    optimizer.step()
+        logits = model(x)
+        loss = compute_loss(logits, y)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()    
+        optimizer.step()
+
+        train_loss += loss.item()
+        n_batches += 1
+
+    train_loss /= n_batches
 
     #------------------------------------------------------
-    if step % 10 == 0 or step == n_iters - 1:
-        x_val, y_val = get_batch('val')
+    if epoch % 1 == 0 or epoch == n_epochs - 1:
         model.eval()
-        with torch.no_grad():
-            logits = model(x_val)
-            val_loss = compute_loss(logits, y_val)
 
-        log_metrics(step, loss.item(), val_loss.item())
+        val_loss = 0.0
+        n_val_batches = 0
+
+        for x_val, y_val in val_loader:
+            x_val, y_val = x_val.to(device), y_val.to(device)
+            with torch.no_grad():
+                logits = model(x_val)
+            val_loss += compute_loss(logits, y_val).item()
+            n_val_batches += 1
+
+        val_loss /= n_val_batches
+
+        log_metrics(epoch, train_loss, val_loss)
     #------------------------------------------------------
-
+    epoch_time = time.perf_counter() - start
+    print(f'Epoch time: {epoch_time:.4f} seconds')
 #------------------------------------------------------
 torch.save(
     {
-    'step': step + 1,
+    'epoch': epoch + 1,
     'model_state_dict': model.state_dict(),
     'optimizer_state_dict': optimizer.state_dict(),
     'model_config': asdict(config),
