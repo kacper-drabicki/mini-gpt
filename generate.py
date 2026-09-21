bffae740848fd63612ee44e5b45ec90a9ec50125
Kacper Drabicki <kdrabicki23@gmail.com>
2026-09-21 14:43:43 +0200

    Wrap in main()

--- generate.py
+++ generate.py
@@ -10,21 +10,25 @@ max_new_tokens = 500
 prompt = '\n'
 #------------------------------------------------------
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dir', required=True)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', required=True)
+    args = parser.parse_args()
 
-experiment_dir = Path(args.dir)
-checkpoint = torch.load(experiment_dir / 'checkpoint_last.pt', map_location=device)
+    experiment_dir = Path(args.dir)
+    checkpoint = torch.load(experiment_dir / 'checkpoint_last.pt', map_location=device)
 
-tokenizer = CharacterTokenizer(checkpoint['tokenizer_chars'])
+    tokenizer = CharacterTokenizer(checkpoint['tokenizer_chars'])
 
-model = MiniGPT(GPTConfig(**checkpoint["model_config"]))
-model.load_state_dict(checkpoint['model_state_dict'])
-model.to(device)
-model.eval()
+    model = MiniGPT(GPTConfig(**checkpoint["model_config"]))
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(device)
+    model.eval()
 
-context = torch.tensor(tokenizer.encode(prompt), dtype=torch.long, device=device).view(1,-1)
-text = model.generate(context, max_new_tokens=max_new_tokens)
-print(tokenizer.decode(text[0].tolist()))
+    context = torch.tensor(tokenizer.encode(prompt), dtype=torch.long, device=device).view(1,-1)
+    text = model.generate(context, max_new_tokens=max_new_tokens)
+    print(tokenizer.decode(text[0].tolist()))
+
+if __name__ == '__main__':
+    main()
 
